315ea4f284f8bf19bca27eb21aba4e45a417f9b4
Nguyen Duy Anh <duyanh19962012@gmail.com>
2020-05-03 00:22:52 +0300

    signed cookies practise

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -1,18 +1,20 @@
 const db = require("../db")
 
 module.exports.requireAuth = (req, res, next) => {
-  if (!req.cookies.userId) {
+  if (!req.signedCookies.userId) {
     console.log("no cookies")
     res.redirect("/auth/login")
     return
   }
 
-  const user = db.get("users").find({ id: req.cookies.userId }).value()
+  const user = db.get("users").find({ id: req.signedCookies.userId }).value()
 
   if (!user) {
     res.redirect("/auth/login")
     return
   }
+
+  res.locals.user = user
 
   next()
 }
